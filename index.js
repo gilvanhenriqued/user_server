329e0134af0557b22ffac71709a1c8af68320dfd
Gilvan Henrique <gilvanh.15@gmail.com>
2020-06-28 17:05:00 -0300

    signup route

--- index.js
+++ index.js
@@ -11,3 +11,27 @@ const db = new sqlite.Database("users.sqlite3");
 
 const KEY = "my y(!!1!11!)<'SECRET>Key'!";
 
+// signup route
+app.post('/signup', express.urlencoded(), function(req, res){
+  let username = req.body.username;
+  let password = req.body.password; 
+
+  password = crypto.createHash('sha256')
+    .update(password)
+    .digest('hex');
+      
+  db.get("SELECT FROM users WHERE username = ?", + [username], function(err, row){
+    if(row != undefined) {
+      console.error("Can't create user " + username);
+      res.status(409);
+      res.send("An user with that username already exists.");
+    } else {
+      console.log("Can create user " + username);
+      db.run('INSERT INTO users(username, password) VALUES (?,?)', [username, password]);
+      res.status(201);
+      res.send("Sucess");
+    }
+  }); 
+});
+
+
